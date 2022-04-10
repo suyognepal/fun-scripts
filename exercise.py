2658244329e2a280f99260c0953f46a6f5ededa9
suyognepal <44490691+suyognepal@users.noreply.github.com>
2022-04-10 12:08:48 +0545

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -10,12 +10,14 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.firefox.options import Options
 import os
 
-def motivation():
+def playyoutube():
     browser = webdriver.Firefox(service_log_path=os.devnull,executable_path=r'.\geckodriver.exe')
-    motivationalvids = "https://www.youtube.com/watch?v=Z63w5PefxTQ&ab_channel=AlexKaltsMotivation" 
+    youtube_video_url = "http://yyoutube url to play" 
     browser = webdriver.Firefox()
     #browser.implicitly_wait(100)
     browser.get(motivationalvids)
     browser.find_element_by_xpath('//*[@id="movie_player"]').click()
     return
-motivation()
+playyoutube()
+
+
